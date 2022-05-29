679cc5b04305a5f16ca0e5d46123f46ebfd16626
albert <inkylin@gmail.com>
2022-05-29 15:23:47 +0800

    add:初始化脚本

---
+++ sql/init.sql
@@ -0,0 +1,12 @@
+-- auto-generated definition
+CREATE TABLE user_info
+(
+    user_id BIGINT AUTO_INCREMENT COMMENT '编号'
+        PRIMARY KEY,
+    name    VARCHAR(20) NULL COMMENT '用户名称',
+    age     INT         NULL COMMENT '年龄',
+    grade   INT         NULL COMMENT '学历',
+    CONSTRAINT user_user_id_uindex
+        UNIQUE (user_id)
+);
+
